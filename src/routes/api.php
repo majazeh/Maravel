1e79d2fadb0dcaa0eb3885069a90a11bbd3753bb
Amir Hossein Jahani <jahani1379@gmail.com>
2019-09-15 11:54:03 +0430

    Add Auth cookie api

--- src/routes/api.php
+++ src/routes/api.php
@@ -1,4 +1,4 @@
 <?php
-Route::group(['middleware' => ['auth:api']], function () {
+Route::group(['middleware' => ['auth:api', 'maravel']], function () {
     Route::apiResource('users', 'UserController', ['as' => 'api']);
 });
