07259e94bd76411c20ef8f07358f01c4f6140ae0
baravak <itb.baravak@gmail.com>
2019-09-15 12:31:43 +0430

    add login, register, logout to api

--- src/Controllers/API/UserController.php
+++ src/Controllers/API/UserController.php
@@ -7,10 +7,12 @@ use App\Requests\Maravel as Request;
 use App\User;
 use Illuminate\Support\Facades\Gate;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Auth\AuthenticationException;
 
 class UserController extends APIController
 {
     public $order_list = ['id', 'name', 'username', 'status', 'type', 'gender'];
+    public $username_method;
 
     public $filters = [
         'test' => true
@@ -111,4 +113,106 @@ class UserController extends APIController
         }
         return [$filters, $current];
     }
+
+    public function login(Request $request)
+	{
+        if (!config('auth.enter.login', true)) {
+            throw new AuthenticationException('login disabled');
+        }
+		$this->username_method($request);
+		if(!\App\User::where($this->username_method, $request->input($this->username_method))->first() && config('auth.enter.auto_register'))
+		{
+			return $this->register($request);
+		}
+		if(\Auth::attempt($this->attempt_rule($request))){
+			$user = $this->show($request, \Auth::user());
+			if($user->status != 'active')
+			{
+				throw new AuthenticationException('not active');
+
+			}
+            $token = $user->createToken('Android')->accessToken;
+            $user->additional(array_merge_recursive($user->additional, [
+                'token' => $token
+            ]));
+            $this->statusMessage = 'succsess';
+			return $user;
+		}
+		else{
+			throw new AuthenticationException('Username or password is not match');
+		}
+	}
+
+	public function attempt_rule(Request $request)
+	{
+		return [
+			$this->username_method => $request->input($this->username_method),
+			'password' => $request->input('password')
+		];
+	}
+
+	public function register(Request $request)
+	{
+		$this->username_method($request);
+		$user = User::where($this->username_method, $request->input($this->username_method))->first();
+		if($user)
+		{
+			return $this->response("user duplicated", null, 401);
+		}
+		$register = new User;
+		$register->password = Hash::make($request->input('password'));
+		$register->{$this->username_method} = $request->input($this->username_method);
+
+		if(config('auth.enter.auto_verify'))
+		{
+			$register->status = 'active';
+		}
+		$register->type = 'user';
+		$register->save();
+		if (config('auth.enter.auto_verify'))
+		{
+            return $this->login($request);
+        }
+        $this->statusMessage = 'registred';
+        return $this->show($request, $this->findOrFail($register->serial ?: $register->id));
+	}
+
+	public function me(Request $request)
+	{
+        $this->statusMessage = 'me';
+        if(!\Auth::user())
+        {
+            throw new AuthenticationException('unauthorized');
+        }
+		return $this->show($request, \Auth::user());
+	}
+
+	public function logout(Request $request)
+	{
+        $user = $this->show($request, \Auth::user());
+        $request->user('api')->token()->revoke();
+        $this->statusMessage = 'logout';
+		return $user;
+	}
+
+	public function username_method(Request $request)
+	{
+		if($this->username_method) return $this->username_method;
+		$username = $request->input('username');
+		$type = 'username';
+		if(ctype_digit($username)){
+			$type = 'mobile';
+			$request->request->remove('username');
+			$request->request->add([$type => $username]);
+		}
+		elseif(strpos($username, '@'))
+		{
+			$type = 'email';
+			$request->request->remove('username');
+			$request->request->add([$type => $username]);
+
+		}
+		$this->username_method = $type;
+		return $type;
+	}
 }
--- src/routes/api.php
+++ src/routes/api.php
@@ -1,4 +1,8 @@
 <?php
 Route::group(['middleware' => ['auth:api', 'maravel']], function () {
+    Route::get('me', 'UserController@me');
     Route::apiResource('users', 'UserController', ['as' => 'api']);
 });
+Route::post('login', 'UserController@login');
+Route::post('logout', 'UserController@logout');
+Route::post('register', 'UserController@register');
