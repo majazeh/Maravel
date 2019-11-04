0932777b7f20dead7011e1c21382204c1f9487ec
Ahmad Karimi <ahmadkarimi1991@gmail.com>
2019-11-04 14:01:46 +0330

    remove extra items from header top-bar

--- src/views/layouts/header/header-topbar.blade.php
+++ src/views/layouts/header/header-topbar.blade.php
@@ -1,11 +1,13 @@
 <div class="kt-header__topbar">
     @section('header-topbar')
-        @include('layouts.header.header-search')
-        @include('layouts.header.header-notifications')
-        @include('layouts.header.header-actions')
-        @include('layouts.header.header-cart')
-        @include('layouts.header.header-panel-toggler')
-        @include('layouts.header.header-language')
+        @if (false)
+            @include('layouts.header.header-search')
+            @include('layouts.header.header-notifications')
+            @include('layouts.header.header-actions')
+            @include('layouts.header.header-cart')
+            @include('layouts.header.header-panel-toggler')
+            @include('layouts.header.header-language')
+        @endif
         @include('layouts.header.header-user')
     @show
 </div>
