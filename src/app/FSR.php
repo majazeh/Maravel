4df62bebc6cc3d59e940bfbf9b575cbaeab79f46
baravak <itb.baravak@gmail.com>
2019-12-09 12:34:20 +0330

    FSR Bugfix

--- src/app/FSR.php
+++ src/app/FSR.php
@@ -13,8 +13,10 @@ class FSR {
 
     public function __toString()
     {
-        $i = floor(log($this->bytes, 1024));
-        return round($this->bytes / pow(1024, $i), [0, 0, 2, 2, 3][$i]) . ' ' . ['b', 'kb', 'mb', 'gb', 'tb'][$i];
+        $size   = array('B', 'kB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB');
+        $factor = floor((strlen($this->bytes) - 1) / 3);
+
+        return sprintf("%.2f ", $this->bytes / pow(1024, $factor)) . @$size[$factor];
     }
 
     public static function make($bytes)
