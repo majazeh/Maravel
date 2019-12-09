5a30350538f1ddb19e2fdd744399fe3cda1e67bd
baravak <itb.baravak@gmail.com>
2019-12-09 11:46:32 +0330

    add File size readable

--- src/app/FSR.php
+++ src/app/FSR.php
@@ -14,8 +14,7 @@ class FSR {
     public function __toString()
     {
         $i = floor(log($this->bytes, 1024));
-        $x = round($this->bytes / pow(1024, $i), [0, 0, 2, 2, 3][$i]) . ' ' . ['b', 'kb', 'mb', 'gb', 'tb'][$i];
-        dd(pow(3, 3));
+        return round($this->bytes / pow(1024, $i), [0, 0, 2, 2, 3][$i]) . ' ' . ['b', 'kb', 'mb', 'gb', 'tb'][$i];
     }
 
     public static function make($bytes)
