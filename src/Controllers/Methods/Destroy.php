55b0112e3623d5c03f1c78dcbf3cd50c6382efd0
baravak <itb.baravak@gmail.com>
2019-12-17 12:08:06 +0330

    bugfix _destroy method

--- src/Controllers/Methods/Destroy.php
+++ src/Controllers/Methods/Destroy.php
@@ -8,7 +8,31 @@ trait Destroy
 {
     public function _destroy(Request $request, $arg1, $arg2 = null)
     {
+        $args = func_get_args();
+        $callback = null;
+        if (last($args) instanceof \Closure) {
+            $callback = last($args);
+            if($arg2 == $callback)
+            {
+                $arg2 = null;
+            }
+        }
         list($parent, $model) = $this->findArgs($request, $arg1, $arg2);
+        if ($callback) {
+            $args = [$request];
+            if($parent)
+            {
+                $args[] = $parent;
+            }
+            $args[] = $model;
+            $result = call_user_func_array($callback, $args);
+            if(is_array($result))
+            {
+                list($parent, $model) = $result;
+            }
+        } else {
+            $model->delete();
+        }
         $result = new $this->resourceClass($model);
         if ($parent) {
             $additional[$this->class_name($this->parentModel, null, 2)] = new $this->parentResourceCollectionClass($parent);
@@ -21,7 +45,6 @@ trait Destroy
             $client = new $this->clientController(...func_get_args());
             $client->webDestroy($request, $result, $arg1, $arg2);
         }
-        $model->delete();
         $this->statusMessage =  $this->class_name() . " removed";
         return $result;
     }
