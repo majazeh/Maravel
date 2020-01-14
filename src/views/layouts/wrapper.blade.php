93fa9dfe1116b5ac4c30fa7697afa6231a8ff89b
Ahmad Karimi <ahmadkarimi1991@gmail.com>
2020-01-14 17:15:06 +0330

    disable footer

--- src/views/layouts/wrapper.blade.php
+++ src/views/layouts/wrapper.blade.php
@@ -1,5 +1,7 @@
 <div class="kt-grid__item kt-grid__item--fluid kt-grid kt-grid--hor kt-wrapper" id="kt_wrapper">
     @include('layouts.header')
     @include('layouts.content')
-    @include('layouts.footer')
+    @if (false)
+        @include('layouts.footer')
+    @endif
 </div>
