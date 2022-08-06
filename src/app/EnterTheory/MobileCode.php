b08ffe9c16bc5224ef44597281a5a48993ef5f9b
Abouhatef <itb.baravak@gmail.com>
2022-08-07 04:05:47 +0430

    mobileCode dynamic expires_at

--- src/app/EnterTheory/MobileCode.php
+++ src/app/EnterTheory/MobileCode.php
@@ -54,7 +54,7 @@ class MobileCode extends Theory
             'value' =>  $value,
             'theory' => 'mobileCode',
             'meta' => ['authorized_key' => $request->mobile ?: $request->authorized_key],
-            'expired_at' => Carbon::now()->addMinutes(5),
+            'expired_at' => isset($parameters['expires_at']) ? $parameters['expires_at'] : Carbon::now()->addMinutes(5),
             'user_id' => isset($parameters['verify_id']) ? $parameters['verify_id'] : null,
             'type' => isset($parameters['verify_id']) ? 'verify' : 'temp'
         ]);
